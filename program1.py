145c9a73158cbcded37be48bf28603eaac683bb3
pratiksontakke123 <92718110+pratiksontakke123@users.noreply.github.com>
2021-10-21 16:43:53 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,6 @@
-import pymysql as mycon
+import  mysql.connector as mycon
 
-con=.connect(host='bzhwycvxazf9gx13focv-mysql.services.clever-cloud.com',user='ujdw7mbcfwibgnu8',password='WwaSxxPbROpjMLle7M8p',database='bzhwycvxazf9gx13focv')
+con=mycon.connect(host='bzhwycvxazf9gx13focv-mysql.services.clever-cloud.com',user='ujdw7mbcfwibgnu8',password='WwaSxxPbROpjMLle7M8p',database='bzhwycvxazf9gx13focv')
 curs=con.cursor()
 
 try:
